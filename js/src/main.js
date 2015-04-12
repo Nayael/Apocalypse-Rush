ff444742ea6f9ab570c4405173544f378c4c7107
Nicolas Vannier <nicolas.vannier93@gmail.com>
2015-04-12 12:04:23 +0200

    Updated > Splashscreen image and canvas background

--- js/src/main.js
+++ js/src/main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var stage = new Stage(Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT, "rgb(0,127,255)");
+    var stage = new Stage(Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT, "rgb(0,0,0)");
 	var application = new Application(stage);
     application.getStage().canvas.style.top = (window.innerHeight / 2 - application.getStage().canvas.height / 2) + 'px';
 
